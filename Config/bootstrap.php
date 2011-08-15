647f69a75299d848a0d6b48926799d1cfd1578de
Fred <fmcell@gmail.com>
2011-08-15 11:04:29 +0800

    Updated version number

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -55,7 +55,7 @@ Configure::write('AclManager.ignoreActions', array('isAuthorized'));
  * END OF USER SETTINGS
  */
 
-Configure::write("AclManager.version", "1.0.0");
+Configure::write("AclManager.version", "1.0.1");
 if (!is_array(Configure::read('AclManager.aros'))) {
 	Configure::write('AclManager.aros', array(Configure::read('AclManager.aros')));
 }
