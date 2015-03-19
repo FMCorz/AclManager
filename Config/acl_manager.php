5a3b55dfc2edf620108312738154a951cb8f3292
houseoftech <corie@houseoftech.com>
2015-03-19 21:53:03 +0100

    Move config variables to config file
    Use plugin's bootstrap file to merge plugin config with local app config.

---
+++ Config/acl_manager.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Acl Manager Config File
+ *
+ * This will be loaded by the AclManger plugin and merged into its configuration
+ *
+ * copy this file to your App/Config/ folder
+ */
+
+$config['AclManager'] = array(
+	/**
+	 * List of AROs (Class aliases)
+	 * Order is important! Parent to Children
+	 */
+	'aros' => array('Role', 'User'),
+	
+	/**
+	 * Aliases to write into ARO table
+	 */
+	// 'aro_aliases' => array('Group' => 'name', 'User' => 'username'),
+	
+	/**
+	 * Limit used to paginate AROs
+	 * Replace {alias} with ARO alias
+	 * '{alias}' => array('limit' => 3)
+	 */
+	// 'Role' => array('limit' => 3),
+	
+	/**
+	 * Routing Prefix
+	 * Set the prefix you would like to restrict the plugin to
+	 * @see Configure::read('Routing.prefixes')
+	 */
+	// 'prefix' => 'admin',
+	
+	/**
+	 * Ugly identation?
+	 * Turn off when using CSS
+	 */
+	'uglyIdent' => true,
+					
+	/**
+	 * Actions to ignore when looking for new ACOs
+	 * Format: 'action', 'Controller/action' or 'Plugin.Controller/action'
+	 */
+	'ignoreActions' => array('isAuthorized'),
+	
+	/**
+	 * List of ARO models to load
+	 * Use only if AclManager.aros aliases are different from model name
+	 */
+	// 'models' => array('Group', 'Customer'),
+	
+	'version' => "1.2.4"
+);
